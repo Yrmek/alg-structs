e4be4efaf4e6de38bce82da7df6629b8eafbc6b3
Hrynkevich Vladislav <yrmek405305@gmail.com>
2024-04-28 17:49:41 +0300

    graphs

--- graph_var4/graph.cpp
+++ graph_var4/graph.cpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -45,19 +47,44 @@ void addTop(Graf*& graf, int topkey)
 		buf->next = newtop;
 	}
 	int num= 0;
-	cout << "Enter numbers of tied tops with top #" << topkey;
+}
 
-	while (cin >> num) {
-		if (num == 0) break;
-		RelNode* buff = new RelNode;
-		buff->key = num;
-		buff->next = nullptr;
-		if (!newtop->relNodes) newtop->relNodes = buff;
-		else {
-			buff->next = newtop->relNodes;
-			newtop->relNodes = buff;
-		}
+void addRelation(Graf*& graf, int key1, int key2)
+{
+	Graf* current = graf;
+	Graf* node1 = nullptr;
+	Graf* node2 = nullptr;
+
+	// Находим вершины с ключами key1 и key2
+	while (current)
+	{
+		if (current->topkey == key1)
+			node1 = current;
+		if (current->topkey == key2)
+			node2 = current;
+
+		current = current->next;
 	}
+
+	// Проверяем, что обе вершины найдены
+	if (!node1 || !node2)
+	{
+		cout << "Одна из вершин не найдена." << endl;
+		return;
+	}
+
+	// Добавляем ребро между вершинами
+	RelNode* newRelNode1 = new RelNode;
+	newRelNode1->key = key2;
+	newRelNode1->next = node1->relNodes;
+	node1->relNodes = newRelNode1;
+
+	RelNode* newRelNode2 = new RelNode;
+	newRelNode2->key = key1;
+	newRelNode2->next = node2->relNodes;
+	node2->relNodes = newRelNode2;
+
+	cout << "Добавлено отношение между вершинами " << key1 << " и " << key2 << endl;
 }
 
 void delTop(Graf*& graf, int topkey)
@@ -94,11 +121,170 @@ void delTop(Graf*& graf, int topkey)
 	}
 }
 
+void printGraph(Graf* graf)
+{
+	Graf* current = graf;
+	while (current)
+	{
+		cout << "Вершина " << current->topkey << ": ";
+		RelNode* relNode = current->relNodes;
+		while (relNode)
+		{
+			cout << relNode->key << " ";
+			relNode = relNode->next;
+		}
+		cout << endl;
+		current = current->next;
+	}
+}
+
+void dfs(Graf* graf, int startKey, bool* visited)
+{
+	Graf* current = graf;
+	while (current && current->topkey != startKey)
+		current = current->next;
+
+	if (!current)
+		return;
+
+	visited[startKey] = true;
+	cout << "Посещена вершина " << startKey << endl;
+
+	RelNode* relNode = current->relNodes;
+	while (relNode)
+	{
+		if (!visited[relNode->key])
+			dfs(graf, relNode->key, visited);
+		relNode = relNode->next;
+	}
+}
+
+void bfs(Graf* graf, int startKey)
+{
+	bool* visited = new bool[100] { false };
+	queue<int> q;
+	visited[startKey] = true;
+	q.push(startKey);
+
+	while (!q.empty())
+	{
+		int currentKey = q.front();
+		q.pop();
+		cout << "Посещена вершина " << currentKey << endl;
+
+		Graf* current = graf;
+		while (current && current->topkey != currentKey)
+			current = current->next;
+
+		if (!current)
+			continue;
+
+		RelNode* relNode = current->relNodes;
+		while (relNode)
+		{
+			if (!visited[relNode->key])
+			{
+				visited[relNode->key] = true;
+				q.push(relNode->key);
+			}
+			relNode = relNode->next;
+		}
+	}
+
+	delete[] visited;
+}
+
+bool dfsFindPath(Graf* graf, int startKey, int endKey, bool* visited)
+{
+	Graf* current = graf;
+	while (current && current->topkey != startKey)
+		current = current->next;
+
+	if (!current)
+		return false;
+
+	visited[startKey] = true;
+
+	if (startKey == endKey)
+	{
+		cout << startKey << " ";
+		return true;
+	}
+
+	RelNode* relNode = current->relNodes;
+	while (relNode)
+	{
+		if (!visited[relNode->key])
+		{
+			if (dfsFindPath(graf, relNode->key, endKey, visited))
+			{
+				cout << startKey << " ";
+				return true;
+			}
+		}
+		relNode = relNode->next;
+	}
+
+	return false;
+}
+
+bool findSimplePathUsingDFS(Graf* graf, int startKey, int endKey)
+{
+	bool* visited = new bool[100] { false };
+	if (!dfsFindPath(graf, startKey, endKey, visited))
+	{
+		cout << "Простой путь между вершинами " << startKey << " и " << endKey << " не найден." << endl;
+		return false;
+	}
+	else {
+		cout << "Путь найден" << endl;
+		return true;
+	}
+	delete[] visited;
+}
+
+
 void main()
 {
-	Graf* graf = NULL; 
+	setlocale(LC_ALL, "");
+	Graf* graf = NULL;
 	addTop(graf, 1);
 	addTop(graf, 2);
 	addTop(graf, 3);
+	addTop(graf, 4);
+	addTop(graf, 5);
+	addTop(graf, 6);
+
+	// Связываем вершины
+	addRelation(graf, 1, 2);
+	addRelation(graf, 1, 3);
+	addRelation(graf, 2, 4);
+	addRelation(graf, 3, 4);
+	addRelation(graf, 4, 5);
+
+	cout << "Граф:" << endl;
+	printGraph(graf);
+
+	cout << "Обход в глубину, начиная с вершины 1:" << endl;
+	bool* visited = new bool[100] { false };
+	dfs(graf, 1, visited);
+	delete[] visited;
+
+	cout << "Обход в ширину, начиная с вершины 1:" << endl;
+	bfs(graf, 1);
+
+	cout << "Поиск пути между вершинами 1 и 5:" << endl;
+	if (!findSimplePathUsingDFS(graf, 1, 5)) {
+		addRelation(graf, 1, 5);
+	}
+	cout << "Поиск пути между вершинами 1 и 6:" << endl;
+	if (!findSimplePathUsingDFS(graf, 1, 6)) {
+		addRelation(graf, 1, 6);
+	}
+	findSimplePathUsingDFS(graf, 1, 5);
+	// Удаление вершины 2
 	delTop(graf, 2);
+
+	cout << "Граф после удаления вершины 2:" << endl;
+	printGraph(graf);
 }
---
+++ matrixgraph_var4/matrixgraph.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct Graf
+{
+    int numVertices;
+    bool** adjMatrix;
+
+    Graf(int n)
+    {
+        numVertices = n;
+        adjMatrix = new bool* [n];
+        for (int i = 0; i < n; i++)
+            adjMatrix[i] = new bool[n] { false };
+    }
+
+    ~Graf()
+    {
+        for (int i = 0; i < numVertices; i++)
+            delete[] adjMatrix[i];
+        delete[] adjMatrix;
+    }
+
+    void addEdge(int v1, int v2)
+    {
+        adjMatrix[v1][v2] = true;
+        adjMatrix[v2][v1] = true;
+    }
+
+    void removeEdge(int v1, int v2)
+    {
+        adjMatrix[v1][v2] = false;
+        adjMatrix[v2][v1] = false;
+    }
+
+    void removeTop(int key)
+    {
+        for (int i = 0; i < this->numVertices; ++i) {
+            adjMatrix[key][i] = false;
+            adjMatrix[i][key] = false;
+        }
+    }
+
+    void dfs(int startKey, bool* visited)
+    {
+        visited[startKey] = true;
+        cout << "Посещена вершина " << startKey << endl;
+
+        for (int i = 0; i < numVertices; i++)
+        {
+            if (adjMatrix[startKey][i] && !visited[i])
+                dfs(i, visited);
+        }
+    }
+
+    void bfs(int startKey)
+    {
+        bool* visited = new bool[numVertices] { false };
+        queue<int> q;
+        visited[startKey] = true;
+        q.push(startKey);
+
+        while (!q.empty())
+        {
+            int currentKey = q.front();
+            q.pop();
+            cout << "Посещена вершина " << currentKey << endl;
+
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (adjMatrix[currentKey][i] && !visited[i])
+                {
+                    visited[i] = true;
+                    q.push(i);
+                }
+            }
+        }
+
+        delete[] visited;
+    }
+
+    bool dfsFindPath(int startKey, int endKey, bool* visited)
+    {
+        visited[startKey] = true;
+
+        if (startKey == endKey)
+        {
+            cout << startKey << " ";
+            return true;
+        }
+
+        for (int i = 0; i < numVertices; i++)
+        {
+            if (adjMatrix[startKey][i] && !visited[i])
+            {
+                if (dfsFindPath(i, endKey, visited))
+                {
+                    cout << startKey << " ";
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool findSimplePathUsingDFS(int startKey, int endKey)
+    {
+        bool* visited = new bool[numVertices] { false };
+        if (!dfsFindPath(startKey, endKey, visited))
+        {
+            cout << "Простой путь между вершинами " << startKey << " и " << endKey << " не найден." << endl;
+            delete[] visited;
+            return false;
+        }
+        else
+        {
+            cout << "Путь найден" << endl;
+            delete[] visited;
+            return true;
+        }
+    }
+
+    void printGraph()
+    {
+        cout << "Граф:" << endl;
+        for (int i = 0; i < this->numVertices; i++)
+        {
+            for (int j = 0; j < this->numVertices; j++)
+            {
+                cout << this->adjMatrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    Graf* graf = new Graf(6);
+
+    graf->addEdge(0, 1);
+    graf->addEdge(0, 2);
+    graf->addEdge(1, 3);
+    graf->addEdge(2, 3);
+    graf->addEdge(3, 4);
+
+    graf->printGraph();
+
+    cout << "Обход в глубину, начиная с вершины 0:" << endl;
+    bool* visited = new bool[graf->numVertices] { false };
+    graf->dfs(0, visited);
+    delete[] visited;
+
+    cout << "Обход в ширину, начиная с вершины 0:" << endl;
+    graf->bfs(0);
+
+    cout << "Поиск пути между вершинами 0 и 4:" << endl;
+    if (!graf->findSimplePathUsingDFS(0, 4))
+    {
+        graf->addEdge(0, 4);
+    }
+
+    cout << "Поиск пути между вершинами 0 и 5:" << endl;
+    if (!graf->findSimplePathUsingDFS(0, 5))
+    {
+        graf->addEdge(0, 5);
+    }
+
+
+    graf->removeEdge(1, 3);
+
+    cout << "Граф после удаления ребра (1, 3):" << endl;
+    graf->printGraph();
+
+    graf->removeTop(3);
+    cout << "Граф после удаления вершины 3:" << endl;
+    graf->printGraph();
+
+    delete graf;
+    return 0;
+}
